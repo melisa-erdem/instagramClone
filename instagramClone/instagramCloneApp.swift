f86c4acff0ef93616e0ffe0671c119346562a497
melisa-erdem <melisaerdem_ceng.github@hotmail.com>
2024-04-26 00:24:36 +0300

    Initial Commit

---
+++ instagramClone/instagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramCloneApp.swift
+//  instagramClone
+//
+//  Created by Melisa Erdem on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct instagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
