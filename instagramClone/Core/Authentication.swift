28f16df310b45990eeaed142b006565410149040
melisa-erdem <melisaerdem_ceng.github@hotmail.com>
2024-04-26 00:30:19 +0300

    new groups
    new groups

---
+++ instagramClone/Core/Authentication.swift
@@ -0,0 +1,18 @@
+//
+//  Authentication.swift
+//  instagramClone
+//
+//  Created by Melisa Erdem on 26.04.2024.
+//
+
+import SwiftUI
+
+struct Authentication: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Authentication()
+}
